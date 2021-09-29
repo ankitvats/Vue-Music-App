4b89d8f9bd24fafce52a52a3f2229ca2061b9ed4
Ankit Vats <ankitvats13@gmail.com>
2021-09-29 16:04:51 +0530

    Added Comment Functionality

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,11 +4,11 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDn8G9X3piWDLg4_YDw-HWzPZ-ixALjThU",
-  authDomain: "music-app-70dc3.firebaseapp.com",
-  projectId: "music-app-70dc3",
-  storageBucket: "music-app-70dc3.appspot.com",
-  appId: "1:1070147429201:web:47429ac58af7fb97fc2970",
+	apiKey: "AIzaSyDn8G9X3piWDLg4_YDw-HWzPZ-ixALjThU",
+	authDomain: "music-app-70dc3.firebaseapp.com",
+	projectId: "music-app-70dc3",
+	storageBucket: "music-app-70dc3.appspot.com",
+	appId: "1:1070147429201:web:47429ac58af7fb97fc2970",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -20,5 +20,6 @@ const storage = firebase.storage();
 // collections - it will select or create if didn't exist
 const usersCollection = db.collection("users");
 const songsCollection = db.collection("songs");
+const commentsCollection = db.collection('comments')
 
-export { auth, db, usersCollection, songsCollection, storage };
+export { auth, db, usersCollection, songsCollection, commentsCollection, storage };
